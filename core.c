38a75eb4e62f4ef8869ef594f6f9d3865a123dab
Zong-Zhe Yang <kevin_yang@realtek.com>
2026-04-04 11:16:52 +0800

    wifi: rtw89: replace RF mutex with wiphy lock assertion
    Now, stack has introduced wiphy lock. And, the normal paths calling
    RF read/write should be under wiphy lock. So, replace RF mutex with
    wiphy lock assertion. Besides, in dbgfs paths, add the corresponding
    lock option.
    
    Signed-off-by: Zong-Zhe Yang <kevin_yang@realtek.com>
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Link: https://patch.msgid.link/20260310080146.31113-11-pkshih@realtek.com

--- core.c
+++ core.c
@@ -6176,7 +6176,6 @@ int rtw89_core_init(struct rtw89_dev *rtwdev)
 		return -ENOMEM;
 	spin_lock_init(&rtwdev->ba_lock);
 	spin_lock_init(&rtwdev->rpwm_lock);
-	mutex_init(&rtwdev->rf_mutex);
 	rtwdev->total_sta_assoc = 0;
 
 	rtw89_init_wait(&rtwdev->mcc.wait);
@@ -6235,7 +6234,6 @@ void rtw89_core_deinit(struct rtw89_dev *rtwdev)
 	__rtw89_fw_free_all_early_h2c(rtwdev);
 
 	destroy_workqueue(rtwdev->txq_wq);
-	mutex_destroy(&rtwdev->rf_mutex);
 }
 EXPORT_SYMBOL(rtw89_core_deinit);
 
